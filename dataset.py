* converted from conv2D to conv3D * added some memory optimisations    -> the training data gets resized along the wrong dimension. So fix that

dataset.py
@@ -2,14 +2,15 @@ import os
 import time
 import nibabel as nib
 import numpy as np
-from torch import from_numpy, save, load, stack, permute
+from torch import from_numpy, save, load, stack
+import torchvision
 from torch.utils.data import Dataset
 
 DATASET_PATH = 'RawData'
 
 
 class BraTSDataset(Dataset):
-    """a class to reprensent a dataset of multi modal MR images"""
+    """a class to represent a dataset of multi-modal MR images"""
     def __init__(self, directory, transform=None):
         self.dir = directory
         self.transform = transform
@@ -37,7 +38,7 @@ class BraTSDataset(Dataset):
         sample = load(item_dir + "/sample.pt")
         mask = load(item_dir + "/mask.pt")
 
-        # check if a transformation has been specified
+        # apply transformation if one has been given
         if self.transform is not None:
             t = self.transform(sample=sample, mask=mask)
             sample = t["sample"]
@@ -53,10 +54,10 @@ def create_dir_list(path):
         if os.path.isdir(d):
             dir_list.append(d)
 
-    print(dir_list)
     # remove entry 355 since it has dodgy filenames
     dir_list.remove(DATASET_PATH + '\\BraTS20_Training_355')
 
+    print("Directory list created...")
     return dir_list
 
 
@@ -66,6 +67,7 @@ def create_ids(dir_list):
         id = dir_list[i][(len(dir_list[i])-3):]
         ids.append(id)
 
+    print("IDs created...")
     return ids
 
 
@@ -86,20 +88,34 @@ def create_dataset():
         nii_mask = nib.nifti1.load(f"{DATASET_PATH}/{current}/{current}_seg.nii")
 
         # convert from nii to tensor
-        flair = from_numpy(np.array(nii_flair.dataobj, dtype='int16'))
-        t1 = from_numpy(np.array(nii_t1.dataobj, dtype='int16'))
-        t1ce = from_numpy(np.array(nii_t1ce.dataobj, dtype='int16'))
-        t2 = from_numpy(np.array(nii_t2.dataobj, dtype='int16'))
-        mask = from_numpy(np.array(nii_mask.dataobj, dtype='int16'))
+        flair = from_numpy(np.array(nii_flair.dataobj, dtype='float32'))
+        t1 = from_numpy(np.array(nii_t1.dataobj, dtype='float32'))
+        t1ce = from_numpy(np.array(nii_t1ce.dataobj, dtype='float32'))
+        t2 = from_numpy(np.array(nii_t2.dataobj, dtype='float32'))
+        mask = from_numpy(np.array(nii_mask.dataobj, dtype='float32'))
+
+        print(f"original shape: {flair.shape}")
+        # reduce resolution (for the sake of memory optimisation)
+        # TODO: currently the data gets resized in the wrong dimensions.
+        #  It gets resized from [240, 240, 155] to [240, 64, 64]. It should be [64, 64, 155]
+        flair_resized = torchvision.transforms.Resize(size=(64, 64))(flair)
+        t1_resized = torchvision.transforms.Resize(size=(64, 64))(t1)
+        t1ce_resized = torchvision.transforms.Resize(size=(64, 64))(t1ce)
+        t2_resized = torchvision.transforms.Resize(size=(64, 64))(t2)
+        mask_resized = torchvision.transforms.Resize(size=(64, 64))(mask)
 
         # combine modalities into a single sample
-        sample = stack((flair, t1, t1ce, t2), 0)
+        sample = stack((flair_resized, t1_resized, t1ce_resized, t2_resized), 0)
+
+        print(f"resized shape: {flair_resized.shape}")
 
         # create directory for each id
         os.mkdir(f"TrainingData/{id}")
         # save tensors in new directory
         save(sample, f"TrainingData/{id}/sample.pt")
-        save(mask, f"TrainingData/{id}/mask.pt")
+        save(mask_resized, f"TrainingData/{id}/mask.pt")
 
     print(f"Dataset created in {time.time()-start_time:.2f} seconds")
 
+
+create_dataset()

model.py
@@ -25,7 +25,7 @@ class DoubleConv(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels=1, features=[64, 128, 256, 512]):
+    def __init__(self, in_channels, out_channels=1, features=[8, 16, 32, 64]):
         super(UNet, self).__init__()
         self.down_samples = nn.ModuleList()
         self.up_samples = nn.ModuleList()
@@ -39,6 +39,9 @@ class UNet(nn.Module):
             )
             in_channels = feature
 
+        # initialise weights
+        self.down_samples.apply(self.init_weights)
+
         ###  Up Sampling  ###
         for feature in reversed(features):
             self.up_samples.append(
@@ -46,8 +49,15 @@ class UNet(nn.Module):
             )
             self.up_samples.append(DoubleConv(feature*2, feature))
 
+        # initialise weights
+        self.up_samples.apply(self.init_weights)
+
         self.bottleneck = DoubleConv(features[-1], features[-1]*2)
         self.final_conv = nn.Conv3d(features[0], out_channels, kernel_size=1)
+
+    def init_weights(self, module):
+        if isinstance(module, nn.Conv3d):
+            nn.init.xavier_uniform_(module.weight)
 
     def forward(self, x):
         skip_connections = []
@@ -67,6 +77,8 @@ class UNet(nn.Module):
 
             # make sure sizes match
             if x.shape != skip_connection.shape:
+                print(f"x: {x.shape}")
+                print(f"skip connections: {skip_connection.shape}")
                 x = TF.resize(x, size=skip_connection.shape[2:], antialias=True)
 
             concat_skip = torch.cat((skip_connection, x), dim=1)
@@ -78,5 +90,5 @@ class UNet(nn.Module):
 def test():
     x = torch.randn((3, 1, 161, 161))
     model = UNet(in_channels=1, out_channels=1)
-    preds = model(x)
-    assert preds.shape == x.shape
+    prediction = model(x)
+    assert prediction.shape == x.shape

train.py
@@ -12,8 +12,8 @@ from model import UNet
 from visualiser import Visualiser
 
 
-class Trainer:
-    """A class that encapsulates the creation and training loop of a model"""
+class TrainingLoop:
+    """A class that encapsulates the creation and training of a UNet segmentation model"""
     def __init__(self, batch, size, channels, epochs, lr, modalities=4):
         """
         Initialise model, dataset, loss function, and any hyperparameters
@@ -25,11 +25,11 @@ class Trainer:
         self.modalities = modalities
         self.epochs = epochs
         self.learning_rate = lr
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"  # determine where the model will be trained
+        self.device = "cpu"  # "cuda" if torch.cuda.is_available() else "cpu"
 
         self.dataloader = self.create_dataloader()  # the 'dataset'
         self.loss = DiceLoss()  # loss function
-        self.model = UNet(in_channels=self.img_channels)  # the UNet model that is being trained
+        self.model = UNet(in_channels=self.img_channels).to(self.device)  # the UNet model that is being trained
         self.optimiser = optim.Adam(params=self.model.parameters(),
                                     lr=self.learning_rate,
                                     betas=(0.9, 0.999),  # these are the amount that the LR will decay by over time
@@ -47,7 +47,8 @@ class Trainer:
                     mean=[0.0, 0.0, 0.0],
                     std=[1.0, 1.0, 1.0]
                 ),
-                transforms.ToTensor()
+                transforms.ToTensor(),
+                transforms.Resize(64)
             ]
         )
         dataset = BraTSDataset('TrainingData')  # TODO: add transforms later
@@ -88,13 +89,17 @@ class Trainer:
                 "==============================================\n"
             )
 
-            for batch_idx, sample in enumerate(self.dataloader):
+            accumulation_steps = 5
 
+            for batch_idx, sample in enumerate(self.dataloader):
+                print(f"batch {batch_idx}...")
                 # TODO: there will likely be some data manipulation needing to be done here
                 # extract the 4 modalities from the sample
                 input = sample[0].to(self.device)
                 # extract the target mask
                 target = sample[1].to(self.device)
+
+                print(f"input shape: {input.shape}")
 
                 # forward pass
                 with torch.cuda.amp.autocast():
@@ -105,7 +110,10 @@ class Trainer:
                 self.scaler.scale(loss).backward()
                 self.scaler.step(self.optimiser)
                 self.scaler.update()
-                self.optimiser.zero_grad()
+
+                if (batch_idx+1) % accumulation_steps == 0:
+                    self.optimiser.step()
+                    self.optimiser.zero_grad()
 
                 # update logs every 10 batches
                 if batch_idx % 10 == 0: