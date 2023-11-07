* fixed problem with dimensions in the wrong order * write up on memory optimisation * investigating why upsampled output is bigger than it should be

dataset.py
@@ -2,7 +2,7 @@ import os
 import time
 import nibabel as nib
 import numpy as np
-from torch import from_numpy, save, load, stack
+from torch import from_numpy, save, load, stack, permute
 import torchvision
 from torch.utils.data import Dataset
 
@@ -71,6 +71,16 @@ def create_ids(dir_list):
     return ids
 
 
+def pre_process(nii):
+    # convert from nii -> numpy -> tensor
+    t = from_numpy(np.array(nii.dataobj, dtype='float32'))
+    # reorder dimensions
+    p = permute(t, (2, 1, 0))
+    # downsize
+    d = torchvision.transforms.Resize(size=(64, 64))(p)
+    return d
+
+
 def create_dataset():
     start_time = time.time()
 
@@ -88,34 +98,23 @@ def create_dataset():
         nii_mask = nib.nifti1.load(f"{DATASET_PATH}/{current}/{current}_seg.nii")
 
         # convert from nii to tensor
-        flair = from_numpy(np.array(nii_flair.dataobj, dtype='float32'))
-        t1 = from_numpy(np.array(nii_t1.dataobj, dtype='float32'))
-        t1ce = from_numpy(np.array(nii_t1ce.dataobj, dtype='float32'))
-        t2 = from_numpy(np.array(nii_t2.dataobj, dtype='float32'))
-        mask = from_numpy(np.array(nii_mask.dataobj, dtype='float32'))
-
-        print(f"original shape: {flair.shape}")
-        # reduce resolution (for the sake of memory optimisation)
-        # TODO: currently the data gets resized in the wrong dimensions.
-        #  It gets resized from [240, 240, 155] to [240, 64, 64]. It should be [64, 64, 155]
-        flair_resized = torchvision.transforms.Resize(size=(64, 64))(flair)
-        t1_resized = torchvision.transforms.Resize(size=(64, 64))(t1)
-        t1ce_resized = torchvision.transforms.Resize(size=(64, 64))(t1ce)
-        t2_resized = torchvision.transforms.Resize(size=(64, 64))(t2)
-        mask_resized = torchvision.transforms.Resize(size=(64, 64))(mask)
+        flair = pre_process(nii_flair)
+        t1 = pre_process(nii_t1)
+        t1ce = pre_process(nii_t1ce)
+        t2 = pre_process(nii_t2)
+        mask = pre_process(nii_mask)
 
         # combine modalities into a single sample
-        sample = stack((flair_resized, t1_resized, t1ce_resized, t2_resized), 0)
-
-        print(f"resized shape: {flair_resized.shape}")
+        sample = stack((flair, t1, t1ce, t2), 0)
 
         # create directory for each id
         os.mkdir(f"TrainingData/{id}")
+
         # save tensors in new directory
         save(sample, f"TrainingData/{id}/sample.pt")
-        save(mask_resized, f"TrainingData/{id}/mask.pt")
+        save(mask, f"TrainingData/{id}/mask.pt")
 
     print(f"Dataset created in {time.time()-start_time:.2f} seconds")
 
 
-create_dataset()
+# create_dataset()

model.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import torchvision.transforms.functional as TF
 
-# TODO: check what the input channels will be for a 3D convolution
-""" will the channels represent the modalities or the number of slices?"""
 
-# TODO: check if the ordering of the input shape matters - if so what is the correct order?
+def init_weights(module):
+    if isinstance(module, nn.Conv3d):
+        nn.init.xavier_uniform_(module.weight)
 
 
 class DoubleConv(nn.Module):
@@ -25,8 +25,10 @@ class DoubleConv(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels=1, features=[8, 16, 32, 64]):
+    def __init__(self, in_channels, out_channels=1, features=None):
         super(UNet, self).__init__()
+        if features is None:
+            features = [16, 32, 64, 128]
         self.down_samples = nn.ModuleList()
         self.up_samples = nn.ModuleList()
         self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
@@ -40,7 +42,7 @@ class UNet(nn.Module):
             in_channels = feature
 
         # initialise weights
-        self.down_samples.apply(self.init_weights)
+        self.down_samples.apply(init_weights)
 
         ###  Up Sampling  ###
         for feature in reversed(features):
@@ -50,14 +52,10 @@ class UNet(nn.Module):
             self.up_samples.append(DoubleConv(feature*2, feature))
 
         # initialise weights
-        self.up_samples.apply(self.init_weights)
+        self.up_samples.apply(init_weights)
 
         self.bottleneck = DoubleConv(features[-1], features[-1]*2)
         self.final_conv = nn.Conv3d(features[0], out_channels, kernel_size=1)
-
-    def init_weights(self, module):
-        if isinstance(module, nn.Conv3d):
-            nn.init.xavier_uniform_(module.weight)
 
     def forward(self, x):
         skip_connections = []
@@ -72,14 +70,18 @@ class UNet(nn.Module):
         skip_connections = skip_connections[::-1]
 
         for idx in range(0, len(self.up_samples), 2):
+            print(f"x (before): {x.shape}")
             x = self.up_samples[idx](x)
+            print(f"x (upsampled): {x.shape}")
             skip_connection = skip_connections[idx//2]
 
             # make sure sizes match
             if x.shape != skip_connection.shape:
-                print(f"x: {x.shape}")
+                print(f"index: {idx}")
+                print(f"up sample: {len(self.up_samples)}")
                 print(f"skip connections: {skip_connection.shape}")
-                x = TF.resize(x, size=skip_connection.shape[2:], antialias=True)
+                x = TF.resize(x, size=[155, 64, 64], antialias=True)
+                print(f"x (resized): {x.shape}")
 
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.up_samples[idx+1](concat_skip)
@@ -88,7 +90,8 @@ class UNet(nn.Module):
 
 
 def test():
-    x = torch.randn((3, 1, 161, 161))
-    model = UNet(in_channels=1, out_channels=1)
+    x = torch.randn((8, 4, 155, 64, 64))
+    model = UNet(in_channels=4)
     prediction = model(x)
     assert prediction.shape == x.shape
+

train.py
@@ -14,7 +14,7 @@ from visualiser import Visualiser
 
 class TrainingLoop:
     """A class that encapsulates the creation and training of a UNet segmentation model"""
-    def __init__(self, batch, size, channels, epochs, lr, modalities=4):
+    def __init__(self, batch, size, channels, epochs, lr):
         """
         Initialise model, dataset, loss function, and any hyperparameters
         that will be used during the training loop
@@ -22,7 +22,6 @@ class TrainingLoop:
         self.batch_size = batch
         self.img_size = size
         self.img_channels = channels
-        self.modalities = modalities
         self.epochs = epochs
         self.learning_rate = lr
         self.device = "cpu"  # "cuda" if torch.cuda.is_available() else "cpu"
@@ -48,7 +47,6 @@ class TrainingLoop:
                     std=[1.0, 1.0, 1.0]
                 ),
                 transforms.ToTensor(),
-                transforms.Resize(64)
             ]
         )
         dataset = BraTSDataset('TrainingData')  # TODO: add transforms later